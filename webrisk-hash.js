2f8424a263aa4b2585d463995c308d8d7f95610e
Igor Rybak <igor@short.io>
2026-03-31 14:34:07 +0300

    fix: correct import usage for getPrefixMap function

--- webrisk-hash.js
+++ webrisk-hash.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { getPrefixMap } from "webrisk-hash";
-const res = webrisk.getPrefixMap(process.argv[2]);
+const res = getPrefixMap(process.argv[2]);
 console.log(res.map(l => `${l[0]} ${l[1].toString("hex")}`).join('\n'));
